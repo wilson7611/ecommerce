b4a5926d999de58ad40d7c6075ce5fa2aa4921a5
wilson7611 <106674260+wilson7611@users.noreply.github.com>
2024-02-19 19:52:02 -0400

    mensaje apra desarrollo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-
+import { API_URL } from "../env"
 
 function App() {
- 
-
   return (
     <>
-      <h1>Hola mundo</h1>
+      <h1>Hola {API_URL}</h1>
+      <p>Este es el sitio en desarrollo</p>
     </>
   )
 }
